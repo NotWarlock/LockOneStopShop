aa37645473034e31d68fb2a875995eb36acc9a35
Sergio Masellis <sergio.masellis@bbva.com>
2017-06-13 08:17:40 -0500

    Type fix #76

--- js/loss.js
+++ js/loss.js
@@ -139,13 +139,13 @@
         } else if ($("nav a:nth-child(3)")[0] !== undefined && $("nav a:nth-child(3)")[0].text === "Sims") {
             request.open('GET', 'guides/' + window.spec + '/Sims/' + itemName + '.html?_=' + new Date().getTime(), true);
             console.log('itemName:', 'guides/' + window.spec + '/Sims/' + encodeURIComponent(itemName) + '.html?_=' + new Date().getTime());
-            $('link[rel="canonical"]').attr('href', 'http://lockonestopsho.com/'+window.spec);
+            $('link[rel="canonical"]').attr('href', 'http://lockonestopshop.com/'+window.spec);
             window.location.hash = "!/" + window.spec + "/Sims/" + encodeURIComponent(itemName);
             window.document.title = "Lock One Stop Shop | " + itemName + " Sims for " + window.spec;
         } else {
             request.open('GET', 'guides/' + window.spec + '/' + itemName + '.html?_=' + new Date().getTime(), true);
             console.log('itemName:', 'guides/' + window.spec + '/' + encodeURIComponent(itemName) + '.html?_=' + new Date().getTime());
-            $('link[rel="canonical"]').attr('href', 'http://lockonestopsho.com/'+window.spec);
+            $('link[rel="canonical"]').attr('href', 'http://lockonestopshop.com/'+window.spec);
             window.location.hash = "!/" + window.spec + "/" + encodeURIComponent(itemName);
             window.document.title = "Lock One Stop Shop | " + itemName + " for " + window.spec;
         }
